ded9f90d7b52f995d12b6aa9a34847f87c56de3a
priyankjain <priyankjjain61@gmail.com>
2014-12-26 00:22:48 +0530

    Object sharing between threads implemented
    Depth functionality not working. Links not getting extracted properly.

--- Broken Links Extractor/Broken Links Extractor/Form1.cs
+++ Broken Links Extractor/Broken Links Extractor/Form1.cs
@@ -6,10 +6,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
+using System.Configuration;
+using HtmlAgilityPack;
 
 namespace Broken_Links_Extractor
 {
@@ -19,21 +22,26 @@ namespace Broken_Links_Extractor
         {
             InitializeComponent();
             fileList = new List<string>();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < max_threads; i++)
                 this.Speed.Items.Add(i + 1);
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < max_depth; i++)
                 this.DepthSelector.Items.Add(i + 1);
         }
         private string fileName = string.Empty;
         private List<string> fileList = null;
         private Queue<string> urlQueue = new Queue<string>();
-        private int threads = 10;
-        private int depth = 2;
-        private int maxQueueSize = 1000;
+        private int threads = int.Parse(ConfigurationManager.AppSettings["Default_Number_Of_Threads"].ToString());
+        private int depth = int.Parse(ConfigurationManager.AppSettings["Default_Depth"].ToString());
+        private int max_depth = int.Parse(ConfigurationManager.AppSettings["Max_Depth"].ToString());
+        private int max_threads = int.Parse(ConfigurationManager.AppSettings["Max_Number_Of_Threads"].ToString());
+        //private int maxQueueSize = 1000;
         static private object waitLock = new object();
         static private object emptyLock = new object();
         int runningThreads = 0;
         static private object outputLock = new object();
+        static private object ouputFileLock = new object();
+        private bool allLinksProcessed = false;
+        static private object[] linkFilesLock = null;
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -43,19 +51,16 @@ namespace Broken_Links_Extractor
         {
 
         }
-
+        StreamWriter[] sw = null;
         private void threadMethod()
         {
             for (; ; )
             {
-                //Monitor.Enter(waitLock);
+                if (allLinksProcessed == true && urlQueue.Count == 0) return;
                 while (urlQueue.Count == 0)
                 {
                     Thread.Sleep(100);
-                    //Monitor.Exit(waitLock);
-                    //Monitor.Wait(emptyLock);
                 }
-                //else Monitor.Exit(waitLock);
                 HttpWebResponse response = null;
                 string url = string.Empty;
                 lock (waitLock)
@@ -66,9 +71,45 @@ namespace Broken_Links_Extractor
                 {
                     HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
                     request.Proxy = null;
-                    request.Timeout = 6000;
+                    request.Timeout = int.Parse(ConfigurationManager.AppSettings["Timeout_In_Seconds"])*1000;
                     request.Method = "GET";
                     response = (HttpWebResponse)request.GetResponse();
+                    StreamReader sr = new StreamReader(response.GetResponseStream());
+                    string html = sr.ReadToEnd();
+                    Uri currentUri = new Uri(url);
+                    Regex linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    HashSet<string> linkList = new HashSet<string>(); 
+                    foreach(Match m in linkParser.Matches(html))
+                    {
+                        Uri muri = null;
+                        lock (outputLock)
+                        {
+                            this.OutputBox.AppendText("Child Link for " + url + ": " + m.Value.ToString() + Environment.NewLine);
+                        }
+                        Uri.TryCreate(m.Value.ToString(),UriKind.RelativeOrAbsolute,out muri);
+                        if(Uri.Compare(muri,currentUri,UriComponents.Host,UriFormat.SafeUnescaped,StringComparison.CurrentCulture) == 0)
+                        {
+                            linkList.Add(m.Value.ToString());
+                        }
+                    }
+                    for(int i=1;i<=this.depth;i++)
+                    {
+                        HashSet<string> links_Of_Current_Depth = new HashSet<string>();
+                        foreach(string matchedUrls in linkList)
+                        {
+                            if(matchedUrls.Replace(@"http\:\/\/","").Replace(@"https\:\/\/","").Split('/').Length - 1 == i )
+                            {
+                                links_Of_Current_Depth.Add(matchedUrls);
+                            }
+                        }
+                        lock(linkFilesLock[i])
+                        {
+                            foreach(string final_Matches in links_Of_Current_Depth)
+                            {
+                                sw[i].WriteLine(final_Matches);
+                            }
+                        }
+                    }
                     lock (outputLock)
                     {
                         this.OutputBox.AppendText(url +" " + Thread.CurrentThread.ManagedThreadId.ToString() + " " + response.StatusDescription + Environment.NewLine);
@@ -88,90 +129,165 @@ namespace Broken_Links_Extractor
                         response.Close();
                         response.Dispose();
                     }
+                    
                 }
             }
         }
         private void StartButton_Click(object sender, EventArgs e)
         {
-            string message = string.Empty;
-            message += this.DepthSelector.ToString() + " " + this.Speed.ToString() + " ";
-            foreach (string file in fileList)
+            allLinksProcessed = false; // A flag which will make all the threads stop when the main thread ends
+
+            #region Spawn the child consumer threads
+            List<Thread> threadList = new List<Thread>();
+            //Spawn the threads
+            for (int i = 0; i < threads; i++)
             {
-                message += file + " ";
+                Thread thread = new Thread(threadMethod);
+                threadList.Add(thread);
+                thread.IsBackground = true;
+                thread.Start();
             }
-            MessageBox.Show(message);
-            //return;
-            StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory()+"/output_"+DateTime.Now.ToString("ddmmyyyyhhMMss")+".txt",true);
-            foreach (string file in fileList)
+            #endregion
+
+            int counter_RunTimeFilesAdded = 0;
+            while (true)
             {
-                StreamReader sr = null;
-                try
+                int inputFileCountBefore = fileList.Count;
+
+                #region Create Shared Objects
+                sw = new StreamWriter[this.depth+1];
+                linkFilesLock = new object[this.depth+1];
+                for (int i = 0; i<=this.depth;i++)
+                {//Open File Handles for the different depths
+                    sw[i] = new StreamWriter(Directory.GetCurrentDirectory()+"/links_depth_" + counter_RunTimeFilesAdded + "_" + i + ".txt",false);
+                    linkFilesLock[i] = new object();
+                }
+                #endregion
+                
+                for(int i=0;i<=this.depth;i++)
                 {
-                    sr = new StreamReader(file);
-                    for (int i = 0; i < threads; i++)
+                    #region Collect links of depth 0
+                    if(i == 0) // For the first depth, collect links from the various input files
                     {
-                        Thread thread = new Thread(threadMethod);
-                        thread.IsBackground = true;
-                        thread.Start();
-                        MessageBox.Show("Thread " + i + " started");
-                    }
-                    while (!sr.EndOfStream)
-                    {
-                        lock(waitLock)
+                        HashSet<String> linkSet = new HashSet<string>();
+                        foreach(string file in fileList)
                         {
-                            while (urlQueue.Count < maxQueueSize && !sr.EndOfStream)
+                            StreamReader sr = null;
+                            try
                             {
-                                urlQueue.Enqueue(sr.ReadLine());
+                                sr = new StreamReader(file);
+                                while(!sr.EndOfStream)
+                                {
+                                    string url = sr.ReadLine();
+                                    Uri uri = null;
+                                    Uri.TryCreate(url,UriKind.Absolute,out uri);
+                                    if(uri == null)
+                                    {
+                                        lock(outputLock)
+                                        {
+                                            this.OutputBox.AppendText("Invalid URL: "+url + " in file: " + file);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        linkSet.Add(url);
+                                    }
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                lock(outputLock)
+                                {
+                                    this.OutputBox.AppendText("Error while opening one of the files: " + ex.Message);
+                                }
+                            }
+                            finally
+                            {
+                                if (sr != null)
+                                    sr.Close();
                             }
                         }
-                        if (sr.EndOfStream)
-                            break;
-                        Monitor.PulseAll(emptyLock);
-                        //Wait till more urls need to be added
-                        while (urlQueue.Count > (int)(maxQueueSize/100))
+                        foreach(string url in linkSet)
                         {
-                            Thread.Sleep(1000);
+                            sw[i].WriteLine(url);
                         }
-                    }
-                }
-                catch (IOException ex)
+                        lock(waitLock)
+                        {
+                            foreach(string url in linkSet)
+                            {
+                                urlQueue.Enqueue(url);
+                            }
+                        }
+                        sw[i].Close();
+                    }//All links of depth 0 written to file
+                    #endregion
+                    
+                }   
+                //foreach (string file in fileList)
+                //{
+                //    StreamReader sr = null;
+                //    try
+                //    {
+                //        sr = new StreamReader(file);
+
+                //        while (!sr.EndOfStream)
+                //        {
+                //            lock (waitLock)
+                //            {
+                //                while (urlQueue.Count < maxQueueSize && !sr.EndOfStream)
+                //                {
+                //                    urlQueue.Enqueue(sr.ReadLine());
+                //                }
+                //            }
+                //            if (sr.EndOfStream)
+                //                break;
+                //            //Monitor.PulseAll(emptyLock);
+                //            //Wait till more urls need to be added
+                //            while (urlQueue.Count > (int)(maxQueueSize / 100))
+                //            {
+                //                Thread.Sleep(1000);
+                //            }
+                //        }
+                //    }
+                //    catch (IOException ex)
+                //    {
+                //        lock (outputLock)
+                //        {
+                //            this.OutputBox.AppendText("Error while opening one of the files: " + ex.Message);
+                //        }
+                //    }
+                //    finally
+                //    {
+                //        if (sr != null)
+                //            sr.Close();
+                //    }
+                //}
+
+                #region Logic to terminate the main thread
+                int inputFileCountAfter = fileList.Count;
+                if (inputFileCountBefore == inputFileCountAfter) break;
+                else
                 {
-                    lock(outputLock)
+                    for (int i = 0; i < inputFileCountBefore; i++)
                     {
-                        this.OutputBox.AppendText("Error while opening one of the files: " + ex.Message);
+                        fileList.RemoveAt(i);
                     }
                 }
-                finally
-                {
-                    if(sr!=null)
-                        sr.Close();
-                }
+                #endregion
+
+            } 
+            #region Logic to terminate and wait for child threads and close shared files
+            allLinksProcessed = true;
+            foreach(Thread t in threadList)
+            {
+                t.Join();
             }
-//            String[] urllist = { "twinpeaks.co.zw",
-//"zanupfpub.co.zw",
-//"dealgooder.com",
-//"touchbaseafrica.co.uk",
-//"zwnews.com",
-//"fxflare.com",
-//"nationalserviceresources.org"};
-//            String message = string.Empty;
-//            foreach(String url in urllist)
-//            {
-//                try
-//                {
-//                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + url);
-//                    request.Method = "GET";
-//                    request.Proxy = null;
-//                    request.KeepAlive = false;
-//                    request.Timeout = 6000;
-//                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-//                }
-//                catch (WebException ex)
-//                {
-//                    message += ex.Response + "\n-" + ex.Message + "\n";
-//                }
-//            }
-//            this.OutputBox.AppendText(message);
+            for(int i=1;i<=this.depth;i++)
+            {
+                if(sw[i]!=null)
+                sw[i].Close();
+            }
+            #endregion
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -190,20 +306,6 @@ namespace Broken_Links_Extractor
                 fileList.Add(this.openFileDialog1.FileName.ToString());
             }
         }
-
-        //void FinishWebRequest(IAsyncResult result)
-        //{
-        //    try
-        //    {
-        //        HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
-        //        Http
-        //        this.OutputBox.AppendText(response.StatusDescription + Environment.NewLine);
-        //    }
-        //    catch (WebException ex)
-        //    {
-        //        this.OutputBox.AppendText(ex.Message + Environment.NewLine);
-        //    }
-        //}
         private void Speed_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.threads = int.Parse(this.Speed.SelectedItem.ToString());
