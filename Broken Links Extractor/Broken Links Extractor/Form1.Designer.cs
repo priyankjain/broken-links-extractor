e5dcfc78d2559db87981e4f2d7c8d1da94a9920a
priyankjain <priyankjjain61@gmail.com>
2014-12-25 18:53:39 +0530

    Multithread top level crawler done

---
+++ Broken Links Extractor/Broken Links Extractor/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Broken_Links_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.Speed = new System.Windows.Forms.ComboBox();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.GroupBox();
+            this.DepthSelector = new System.Windows.Forms.ComboBox();
+            this.Actions = new System.Windows.Forms.GroupBox();
+            this.Options = new System.Windows.Forms.GroupBox();
+            this.Output.SuspendLayout();
+            this.Actions.SuspendLayout();
+            this.Options.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Upload Links File";
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.OutputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputBox.Location = new System.Drawing.Point(6, 16);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(903, 161);
+            this.OutputBox.TabIndex = 2;
+            this.OutputBox.Text = "";
+            this.OutputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // Speed
+            // 
+            this.Speed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Speed.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Speed.FormattingEnabled = true;
+            this.Speed.Location = new System.Drawing.Point(137, 16);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(87, 21);
+            this.Speed.TabIndex = 4;
+            this.Speed.Text = "Speed";
+            this.Speed.SelectedIndexChanged += new System.EventHandler(this.Speed_SelectedIndexChanged);
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.UploadButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UploadButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.UploadButton.Location = new System.Drawing.Point(3, 16);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(77, 29);
+            this.UploadButton.TabIndex = 3;
+            this.UploadButton.Text = "Upload";
+            this.UploadButton.UseVisualStyleBackColor = false;
+            this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.StartButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.StartButton.Location = new System.Drawing.Point(145, 16);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(87, 29);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Output
+            // 
+            this.Output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Output.Controls.Add(this.OutputBox);
+            this.Output.Location = new System.Drawing.Point(12, 76);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(915, 183);
+            this.Output.TabIndex = 5;
+            this.Output.TabStop = false;
+            this.Output.Text = "Output";
+            this.Output.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // DepthSelector
+            // 
+            this.DepthSelector.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DepthSelector.Dock = System.Windows.Forms.DockStyle.Left;
+            this.DepthSelector.FormattingEnabled = true;
+            this.DepthSelector.Location = new System.Drawing.Point(3, 16);
+            this.DepthSelector.Name = "DepthSelector";
+            this.DepthSelector.Size = new System.Drawing.Size(77, 21);
+            this.DepthSelector.TabIndex = 6;
+            this.DepthSelector.Text = "Depth";
+            this.DepthSelector.SelectedIndexChanged += new System.EventHandler(this.Depth_SelectedIndexChanged);
+            // 
+            // Actions
+            // 
+            this.Actions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Actions.Controls.Add(this.UploadButton);
+            this.Actions.Controls.Add(this.StartButton);
+            this.Actions.Location = new System.Drawing.Point(692, 3);
+            this.Actions.Name = "Actions";
+            this.Actions.Size = new System.Drawing.Size(235, 48);
+            this.Actions.TabIndex = 7;
+            this.Actions.TabStop = false;
+            this.Actions.Text = "Actions";
+            this.Actions.Enter += new System.EventHandler(this.groupBox1_Enter_1);
+            // 
+            // Options
+            // 
+            this.Options.Controls.Add(this.Speed);
+            this.Options.Controls.Add(this.DepthSelector);
+            this.Options.Location = new System.Drawing.Point(12, 3);
+            this.Options.Name = "Options";
+            this.Options.Size = new System.Drawing.Size(227, 48);
+            this.Options.TabIndex = 8;
+            this.Options.TabStop = false;
+            this.Options.Text = "Options";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 271);
+            this.Controls.Add(this.Options);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Actions);
+            this.Name = "Form1";
+            this.Text = "Broken Links Extractor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.Output.ResumeLayout(false);
+            this.Actions.ResumeLayout(false);
+            this.Options.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.ComboBox Speed;
+        private System.Windows.Forms.Button UploadButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.GroupBox Output;
+        private System.Windows.Forms.ComboBox DepthSelector;
+        private System.Windows.Forms.GroupBox Actions;
+        private System.Windows.Forms.GroupBox Options;
+    }
+}
+
---
+++ Broken Links Extractor/Broken Links Extractor/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Broken_Links_Extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fileList = new List<string>();
+            for (int i = 0; i < 100; i++)
+                this.Speed.Items.Add(i + 1);
+            for (int i = 0; i < 5; i++)
+                this.DepthSelector.Items.Add(i + 1);
+        }
+        private string fileName = string.Empty;
+        private List<string> fileList = null;
+        private Queue<string> urlQueue = new Queue<string>();
+        private int threads = 10;
+        private int depth = 2;
+        private int maxQueueSize = 1000;
+        static private object waitLock = new object();
+        static private object emptyLock = new object();
+        int runningThreads = 0;
+        static private object outputLock = new object();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void threadMethod()
+        {
+            for (; ; )
+            {
+                //Monitor.Enter(waitLock);
+                while (urlQueue.Count == 0)
+                {
+                    Thread.Sleep(100);
+                    //Monitor.Exit(waitLock);
+                    //Monitor.Wait(emptyLock);
+                }
+                //else Monitor.Exit(waitLock);
+                HttpWebResponse response = null;
+                string url = string.Empty;
+                lock (waitLock)
+                {
+                    url = urlQueue.Dequeue();
+                }
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                    request.Proxy = null;
+                    request.Timeout = 6000;
+                    request.Method = "GET";
+                    response = (HttpWebResponse)request.GetResponse();
+                    lock (outputLock)
+                    {
+                        this.OutputBox.AppendText(url +" " + Thread.CurrentThread.ManagedThreadId.ToString() + " " + response.StatusDescription + Environment.NewLine);
+                    }
+                }
+                catch (WebException ex)
+                {
+                    lock (outputLock)
+                    {
+                        this.OutputBox.AppendText(url + " " + Thread.CurrentThread.ManagedThreadId.ToString() + " " + ex.Message + Environment.NewLine);
+                    }
+                }
+                finally
+                {
+                    if (response != null)
+                    {
+                        response.Close();
+                        response.Dispose();
+                    }
+                }
+            }
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string message = string.Empty;
+            message += this.DepthSelector.ToString() + " " + this.Speed.ToString() + " ";
+            foreach (string file in fileList)
+            {
+                message += file + " ";
+            }
+            MessageBox.Show(message);
+            //return;
+            StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory()+"/output_"+DateTime.Now.ToString("ddmmyyyyhhMMss")+".txt",true);
+            foreach (string file in fileList)
+            {
+                StreamReader sr = null;
+                try
+                {
+                    sr = new StreamReader(file);
+                    for (int i = 0; i < threads; i++)
+                    {
+                        Thread thread = new Thread(threadMethod);
+                        thread.IsBackground = true;
+                        thread.Start();
+                        MessageBox.Show("Thread " + i + " started");
+                    }
+                    while (!sr.EndOfStream)
+                    {
+                        lock(waitLock)
+                        {
+                            while (urlQueue.Count < maxQueueSize && !sr.EndOfStream)
+                            {
+                                urlQueue.Enqueue(sr.ReadLine());
+                            }
+                        }
+                        if (sr.EndOfStream)
+                            break;
+                        Monitor.PulseAll(emptyLock);
+                        //Wait till more urls need to be added
+                        while (urlQueue.Count > (int)(maxQueueSize/100))
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    lock(outputLock)
+                    {
+                        this.OutputBox.AppendText("Error while opening one of the files: " + ex.Message);
+                    }
+                }
+                finally
+                {
+                    if(sr!=null)
+                        sr.Close();
+                }
+            }
+//            String[] urllist = { "twinpeaks.co.zw",
+//"zanupfpub.co.zw",
+//"dealgooder.com",
+//"touchbaseafrica.co.uk",
+//"zwnews.com",
+//"fxflare.com",
+//"nationalserviceresources.org"};
+//            String message = string.Empty;
+//            foreach(String url in urllist)
+//            {
+//                try
+//                {
+//                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + url);
+//                    request.Method = "GET";
+//                    request.Proxy = null;
+//                    request.KeepAlive = false;
+//                    request.Timeout = 6000;
+//                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+//                }
+//                catch (WebException ex)
+//                {
+//                    message += ex.Response + "\n-" + ex.Message + "\n";
+//                }
+//            }
+//            this.OutputBox.AppendText(message);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+        }
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UploadButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                fileList.Add(this.openFileDialog1.FileName.ToString());
+            }
+        }
+
+        //void FinishWebRequest(IAsyncResult result)
+        //{
+        //    try
+        //    {
+        //        HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
+        //        Http
+        //        this.OutputBox.AppendText(response.StatusDescription + Environment.NewLine);
+        //    }
+        //    catch (WebException ex)
+        //    {
+        //        this.OutputBox.AppendText(ex.Message + Environment.NewLine);
+        //    }
+        //}
+        private void Speed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.threads = int.Parse(this.Speed.SelectedItem.ToString());
+        }
+
+        private void Depth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.depth = int.Parse(this.DepthSelector.SelectedItem.ToString());
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
